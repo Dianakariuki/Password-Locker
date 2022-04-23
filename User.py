1efeeaa696c22be135cd7f6a43471b529c426795
Diana Kariuki <dianakariuki842@gmail.com>
2022-04-23 16:11:09 +0300

    This commit creates an init method to create new instances of a classs

--- User.py
+++ User.py
@@ -3,4 +3,8 @@ class User:
     class that creates instances of users
     '''
     
-users_list = [] #creates an empty list for us
+users_list = [] 
+
+def __init__(self, user_name, user_passwrd):
+        self.user_name = user_name
+        self.user_passwrd = user_passwrd
