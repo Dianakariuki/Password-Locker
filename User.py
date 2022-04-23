f4bf2247c1b5505e73b4a8ca0081181618f85b65
Diana Kariuki <dianakariuki842@gmail.com>
2022-04-23 16:15:29 +0300

    This ommit creates an append method to add in the saved usercredentials

--- User.py
+++ User.py
@@ -8,3 +8,8 @@ users_list = []
 def __init__(self, user_name, user_passwrd):
         self.user_name = user_name
         self.user_passwrd = user_passwrd
+def save_user(self):
+        '''
+        This method saves new object to users list
+        '''
+        User.users_list.append(self)
