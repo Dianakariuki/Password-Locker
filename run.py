5c3d703c5e7ad1ccdd7cb48222f4e613d4c7bc27
Diana Kariuki <dianakariuki842@gmail.com>
2022-04-24 01:48:26 +0300

    This commit creates a function for creating a lass

--- run.py
+++ run.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3.8
 from credentials import Password
+
+
+def create_credential(sname, fname, lname, password):
+    '''
+    Function to create a new user credentials
+    '''
+    new_credential = Password(sname, fname, lname,password)
+    return new_credential
